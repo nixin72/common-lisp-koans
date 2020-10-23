eb027931d6809d63d89bde9d5750eb20147825fd
Philip Dumaresq <phdumaresq@protonmail.com>
2020-10-23 00:56:43 -0400

    99/199 Common Lisp koans

---
+++ meditate-macos.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ $# != 1 ]; then
+    echo "usage: sh meditate.sh <lisp implementation>"
+    echo "       lisp implementation: one of abcl, ccl, clisp, ecl, or sbcl"
+    exit
+fi
+
+choose_command_line() {
+    case "$1" in
+        'abcl' )
+            echo "abcl --noinform --noinit --load contemplate.lisp --eval '(quit)'"
+        ;;
+        'ccl' )
+            echo "ccl -n -l contemplate.lisp -e '(quit)'"
+        ;;
+        'clisp' )
+            echo "clisp -q -norc -ansi contemplate.lisp"
+        ;;
+        'ecl' )
+            echo "ecl -norc -load contemplate.lisp -eval '(quit)'"
+        ;;
+        'sbcl' )
+            echo "sbcl --script contemplate.lisp"
+        ;;
+        * )
+            echo ""
+            exit
+            ;;
+    esac
+}
+
+CONTEMPLATE=$(choose_command_line $1)
+if [ "$CONTEMPLATE" = "" ]; then
+    echo "Unknown Lisp implementation."
+    exit
+else
+    echo $CONTEMPLATE
+fi
+
+$CONTEMPLATE
+while fswatch --exclude '#.*#' -r1 koans; do
+    $CONTEMPLATE
+done
